889d373d6055a21bc3b240d76d6d269c9fced370
a25davmunley <a25davmunley@inspedralbes.cat>
2026-04-20 18:39:18 +0200

    add

---
+++ Projecte MegaMan/Assets/Scripts/characters/player/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour, PlayerInputAction.IPlayerActions
+{
+    private PlayerInputAction inputActions;
+    private Rigidbody2D rb;
+
+    public float speed = 5f;
+    public float jumpForce = 7f;
+
+    private float moveX;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputAction();
+        inputActions.Player.SetCallbacks(this);
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void Update()
+    {
+        CheckGround();
+        Move();
+        
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+    }
+
+    private void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundRadius,
+            groundLayer
+        );
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveX = context.ReadValue<Vector2>().x;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    public void OnPausa(InputAction.CallbackContext context) { }
+    public void OnPaneldepoderes(InputAction.CallbackContext context) { }
+    public void OnPoderes(InputAction.CallbackContext context) { }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+    }
+}
---
+++ Projecte MegaMan/Assets/Scripts/characters/player/WeaponSystem.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponSystem : MonoBehaviour
+{
+    public enum WeaponType
+    {
+        Buster,
+        Fire,
+        Wind,
+        Thunder
+    }
+
+    private WeaponType currentWeapon;
+    private WeaponType[] weapons;
+    private int currentIndex = 0;
+
+    private void Start()
+    {
+        weapons = new WeaponType[]
+        {
+            WeaponType.Buster,
+            WeaponType.Fire,
+            WeaponType.Wind,
+            WeaponType.Thunder
+        };
+
+        currentWeapon = weapons[currentIndex];
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.eKey.wasPressedThisFrame)
+            NextWeapon();
+
+        if (Keyboard.current.qKey.wasPressedThisFrame)
+            PreviousWeapon();
+
+        if (Keyboard.current.fKey.wasPressedThisFrame)
+            Shoot();
+    }
+
+    void NextWeapon()
+    {
+        currentIndex++;
+
+        if (currentIndex >= weapons.Length)
+            currentIndex = 0;
+
+        currentWeapon = weapons[currentIndex];
+        Debug.Log("Arma: " + currentWeapon);
+    }
+
+    void PreviousWeapon()
+    {
+        currentIndex--;
+
+        if (currentIndex < 0)
+            currentIndex = weapons.Length - 1;
+
+        currentWeapon = weapons[currentIndex];
+        Debug.Log("Arma: " + currentWeapon);
+    }
+
+    void Shoot()
+    {
+        switch (currentWeapon)
+        {
+            case WeaponType.Buster:
+                Debug.Log("Disparo normal");
+                break;
+
+            case WeaponType.Fire:
+                Debug.Log("Fuego 🔥");
+                break;
+
+            case WeaponType.Wind:
+                Debug.Log("Wind");
+                break;
+
+            case WeaponType.Thunder:
+                Debug.Log("Rayo ⚡");
+                break;
+        }
+    }
+}
